84d1664d5dfb3eaee2b7d959b08341985465ed06
develop_phayuphat <develop_phayuphat.t@xsurface.com>
2023-06-30 14:13:46 +0700

    [asf]

--- app/(home)/(sections)/section_image.tsx
+++ app/(home)/(sections)/section_image.tsx
@@ -3,74 +3,82 @@ import { motion, useInView, useScroll, useTransform } from "framer-motion";
 import { useEffect, useRef } from "react";
 
 const SectionImage = () => {
-  const { scrollYProgress } = useScroll();
-  const backgroundPositionY = useTransform(
-    scrollYProgress,
-    [0, 1],
-    ["200px", "-200px"]
-  );
-  const containerRef = useRef<HTMLDivElement | null>(null);
-  const imageInView = useInView(containerRef, { amount: 0.5, once:true });
+    const containerRef = useRef<HTMLDivElement | null>(null);
+    const containerSecondRef = useRef<HTMLDivElement | null>(null);
+    const imageInView = useInView(containerRef, { amount: 0.5, once: true });
+    const imageBiggerInView = useInView(containerSecondRef, {
+        amount: 0.5,
+        once: true,
+    });
 
-  return (
-    <div className="flex h-screen w-full flex-col gap-5 px-[3%] lg:h-[1611px]">
-      <div className="flex h-[50%] gap-5 lg:h-[873px]" ref={containerRef}>
-        <motion.div className="basis-1/2 overflow-hidden rounded-[20px]">
-          <motion.div
-            animate={{
-              height: imageInView ? `100%` : `0%`,
-              backgroundSize: imageInView ? "130%" : "100%",
-            }}
-            transition={{ duration: 1 }}
-            className="h-full w-full bg-black bg-biko-a "
-          >
+    const variant = {
+        show: {
+            height: imageInView ? `100%` : `0%`,
+            backgroundSize: imageInView ? "130%" : "100%",
+        },
+    };
+
+    const variant_bg = {
+        hidden: { backdropFilter: "blur(5px)" },
+        show: { backdropFilter: imageInView ? "blur(0px)" : "blur(5px)" },
+    };
+
+    const variant_full_bg = {
+        hidden: { backdropFilter: "blur(5px)" },
+        show: { backdropFilter: imageBiggerInView ? "blur(0px)" : "blur(5px)" },
+    };
+
+    return (
+        <div className="flex h-screen w-full flex-col gap-5 px-[3%] lg:h-[1611px]">
+            <div className="flex h-[50%] gap-5 lg:h-[873px]" ref={containerRef}>
+                <motion.div className="basis-1/2 overflow-hidden rounded-[20px]">
+                    <motion.div
+                        variants={variant}
+                        transition={{ duration: 1 }}
+                        animate="show"
+                        className="h-full w-full bg-black bg-biko-a "
+                    >
+                        <motion.div
+                            variants={variant_bg}
+                            initial="hidden"
+                            animate="show"
+                            transition={{ duration: 1.5 }}
+                            className="h-full w-full "
+                        ></motion.div>
+                    </motion.div>
+                </motion.div>
+                <motion.div className="basis-1/2 overflow-hidden rounded-[20px]">
+                    <motion.div
+                        variants={variant}
+                        animate="show"
+                        transition={{ duration: 1 }}
+                        className="h-full w-full bg-black bg-biko-b"
+                    >
+                        <motion.div
+                            variants={variant_bg}
+                            initial="hidden"
+                            animate="show"
+                            transition={{ duration: 1.5 }}
+                            className="h-full w-full "
+                        ></motion.div>
+                    </motion.div>
+                </motion.div>
+            </div>
             <motion.div
-              initial={{ backdropFilter: "blur(5px)" }}
-              animate={{
-                backdropFilter: imageInView ? "blur(0px)" : "blur(5px)",
-              }}
-              transition={{ duration: 1.5 }}
-              className="h-full w-full "
-            ></motion.div>
-          </motion.div>
-        </motion.div>
-        <motion.div className="basis-1/2 overflow-hidden rounded-[20px]">
-          <motion.div
-            animate={{
-              height: imageInView ? `100%` : `0%`,
-              backgroundSize: imageInView ? "130%" : "100%",
-            }}
-            transition={{ duration: 1 }}
-            className="h-full w-full bg-black bg-biko-b"
-          >
-            <motion.div
-              initial={{ backdropFilter: "blur(5px)" }}
-              animate={{
-                backdropFilter: imageInView ? "blur(0px)" : "blur(5px)",
-              }}
-              transition={{ duration: 1.5 }}
-              className="h-full w-full "
-            ></motion.div>
-          </motion.div>
-        </motion.div>
-      </div>
-      <motion.div className="h-[50%] w-full overflow-hidden rounded-[20px] lg:h-[712px]">
-        <motion.div
-          initial={{ scale: "150%" }}
-          style={{ backgroundPositionY }}
-          className="h-full w-full bg-black bg-biko-c bg-cover"
-        >
-          <motion.div
-            initial={{ backdropFilter: "blur(5px)" }}
-            transition={{ duration: 0.6, ease: "easeOut" }}
-            viewport={{ amount: 0.3, once: true }}
-            whileInView={{ backdropFilter: "blur(0px)" }}
-            className="h-full w-full "
-          ></motion.div>
-        </motion.div>
-      </motion.div>
-    </div>
-  );
+                className="h-[50%] w-full overflow-hidden rounded-[20px] lg:h-[712px]"
+                ref={containerSecondRef}
+            >
+                <motion.div className="h-full w-full bg-black bg-biko-c bg-cover">
+                    <motion.div
+                        variants={variant_full_bg}
+                        initial="hidden"
+                        animate="show"
+                        className="h-full w-full "
+                    ></motion.div>
+                </motion.div>
+            </motion.div>
+        </div>
+    );
 };
 
 export default SectionImage;
